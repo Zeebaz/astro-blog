4db446fc59b6e5b1123ad7d1e73a7b6abe02e7f0
Zeebaz <jairosebastianrp@gmail.com>
2024-03-12 17:30:33 -0600

    client script added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://astro-initials.netlify.app"
+  site: "https://astro-initials.netlify.app",
+  integrations: [preact()]
 });
