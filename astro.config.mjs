0001613e41dbf6d7041d70b033589427996cab7e
Zeebaz <jairosebastianrp@gmail.com>
2024-03-14 14:49:57 -0600

    Add internationalization settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,14 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   site: "https://astro-initials.netlify.app",
-  integrations: [preact()]
+  integrations: [preact()],
+  i18n: {
+    defaultLocale: "es",
+    locales: ["en", "es"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
